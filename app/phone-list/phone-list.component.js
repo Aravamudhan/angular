7374c7fcf8f8e2013942d0263d0e3d21375bc4a7
amudhan-mumms <amudhan.a@mumms.com>
2016-09-28 18:12:07 +0530

    dependency injection, data binding

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -1,32 +1,32 @@
 // The component phoneList is being attached to the module phoneCatAppModule
 angular.
-  module('phoneListModule').
-    component('phoneList',{
-     //The URL is relative to the home directory or the index.html file.
-     //Using template directly couples this file with the HTML. By Using
-     //templateUrl instead of template code is cleaner and decoupled from HTML.
-     //For every templateUrl, a request is made to the server. Angular provides lazy fetching of templates
-     //and caching to make it effective, especially on mobile devices.
-      templateUrl : 'phone-list/phone-list.template.html',
-
-      controller : function PhoneListController(){
-        this.name = 'world';
-        this.rowNumbers = [0, 1, 2, 3, 4, 5, 6, 7];
-        this.columnNumbers = [0, 1, 2, 3, 4, 5, 6, 7];
-        this.phones = [
-        {
-          name: 'Nexus S',
-          snippet: 'Fast just got faster with Nexus S.'
-        },
-        {
-          name: 'Motorola XOOM™ with Wi-Fi',
-          snippet: 'The Next, Next Generation tablet.'
-        },
-        {
-          name: 'MOTOROLA XOOM™',
-          snippet: 'The Next, Next Generation tablet.'
+module('phoneListModule').
+component('phoneList', {
+    //The URL is relative to the home directory or the index.html file.
+    //Using template directly couples this file with the HTML. By Using
+    //templateUrl instead of template code is cleaner and decoupled from HTML.
+    //For every templateUrl, a request is made to the server. Angular provides lazy fetching of templates
+    //and caching to make it effective, especially on mobile devices.
+    templateUrl: 'phone-list/phone-list.template.html',
+    //1. To use a service declare the dependencies in the controller's
+    //constructor. They will be injected through DI.
+    //2. It is good enough to declare dependency this way ."controller: function PhoneListController($http) {}"
+    //But during minification this might get lost and angular will have no way to identify the dependencies
+    //to be injected to that constructor. To avoid this by setting the controller property as an array,
+    //which starts with the dependencies its requires, then going on to declare the controller function
+    //as its callback prevents this possible problem.
+    //3. Another syntax is declaring the method separately and passing that along with its dependencies list
+    //to the controller.
+    //function PhoneListController($http) {...}
+    //...
+    //.component('phoneList', {..., controller: ['$http', PhoneListController]});
+    controller: ['$http',
+        function PhoneListController($http) {
+            var self = this;
+            self.orderProp = 'age';
+            $http.get('/phones/phones.json').then(function(response) {
+                self.phones = response.data;
+            });
         }
-      ];
-      }
-
-    });
+    ]
+});
--- app/phone-list/phone-list.module.js
+++ app/phone-list/phone-list.module.js
@@ -1,3 +1,2 @@
-// Initializing phoneCatApp module
 // For an app, a module is created and controllers are attached to it.
 angular.module('phoneListModule', []);
